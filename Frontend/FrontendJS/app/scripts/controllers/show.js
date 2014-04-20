c2383788024f40d2f1f17f5a03b5cb7b7536c60a
lauraliparulo <tigertailz83>
2014-04-20 13:55:39 +0200

    show ctrl

--- Frontend/FrontendJS/app/scripts/controllers/show.js
+++ Frontend/FrontendJS/app/scripts/controllers/show.js
@@ -1,11 +1,48 @@
 'use strict';
 
 angular.module('employeesJsApp')
+    .constant('baseUrl', '/employees-manager/api/rest/')
     .controller('ShowCtrl', function ($scope) {
-        $scope.employees = [
+
+        $scope.employees = [];
+        $scope.noDataFoundMessage=undefined;
+        $scope.noLinkFoundMessage=undefined;
+
+        $scope.employees_ = [
             {name:'laura'},
             {name:'andreas'},
             {name:'salva'}
         ];
-    });
+
+        $scope.getAllEmployees = function (){
+            searchService.getEmployeesList()
+
+                .success(function(data) {
+
+                    if (data.success !== false) {
+                        $scope.noDataFoundMessage=undefined;
+                        $scope.employees = data;
+                    }
+                    else {
+                        $scope.noDataFoundMessage=data.message;
+                        $scope.employees = data;
+                    }
+                })
+                        .error(function(data) {
+
+                    $scope.noLinkFoundMessage=data.message;
+                        });
+
+
+    }})
+
+.service('searchService', ['$http', 'baseUrl', function ($http, baseUrl) {
+    this.getEmployeesList = function() {
+        return $http.get(baseUrl +'all') ;
+    };
+}]);
+
+/*
+{"name" : { "first" : "Mike", "last" : "Patton" }, "street" : "Sunset Boulevard 55", "age" : 53, "phone" : "555 123237", "skills" : [ "C++", "PHP", "jquery" ] }
+*/
 
